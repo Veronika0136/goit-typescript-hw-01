2f9518274719abb51f0e2725423f473ef81c9b27
veronika <artemova331@gmail.com>
2025-06-19 21:18:04 +0300

    add tasks

---
+++ src/basic/3.ts
@@ -0,0 +1,4 @@
+const a: string | number = "Hello";
+
+type State = "enable" | "disable";
+const state: State = "enable";
--- src/basic/4.ts
+++ src/basic/4.ts
@@ -1,8 +1,8 @@
-function showMessage(message) {
+function showMessage(message: string): void {
   console.log(message);
 }
 
-function calc(num1, num2) {
+function calc(num1: number, num2: number): number {
   return num1 + num2;
 }
 
